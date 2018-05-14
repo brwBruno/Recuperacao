281e7d406fba06b974a7f1a5e0d1e8b84a20d306
Brwbruno01 <bruno_brw@hotmail.com>
2018-05-14 00:27:02 -0300

    Caminho feliz de teste de sistema pronto

--- DonaLaura/DonaLaura.Infra/Base/ExtensionSql.cs
+++ DonaLaura/DonaLaura.Infra/Base/ExtensionSql.cs
@@ -9,7 +9,7 @@ namespace DonaLaura.Infra.Base
     public static class ExtensionSql
     {
         private static string truncate = "TRUNCATE TABLE TBProduct";
-        private static string add = "INSERT INTO TBProduct (NOME, PRECO_VENDA, PRECO_CUSTO, DISPONIBILIDADE,DATA_FAB, DATA_VAL) VALUES ('Bruno',  10.50, 8.50, 1, 2018-11-05,2018-13-05)";
+        private static string add = "INSERT INTO TBProduct (NOME, PRECO_VENDA, PRECO_CUSTO, DISPONIBILIDADE,DATA_FAB, DATA_VAL) VALUES ('Creme Facial',  10.50, 8.50, 1, 2018-11-05,2018-13-05)";
 
         public static void SeedDb()
         {
