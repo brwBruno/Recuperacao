8bd028058433279e4af1937133d06575e46899f4
Bruno Wagner <brunowagner62@gmail.com>
2018-05-15 17:11:00 -0300

    Implementado dominio de venda e testada 100%

---
+++ DonaLaura/DonaLaura.Domain.Tests/Features/Sales/SaleTests.cs
@@ -0,0 +1,81 @@
+﻿using DonaLaura.Common.Tests.Base;
+using DonaLaura.Domain.Features.Products;
+using DonaLaura.Domain.Features.Sales;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonaLaura.Domain.Tests.Features.Sales
+{
+    [TestFixture]
+    public class SaleTests
+    {
+        Sale _sale;
+        Mock<Product> _productSale;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sale = new Sale();
+            _productSale = new Mock<Product>();
+        }
+
+        [Test]
+        public void Sale_ValidSale_ShouldBeOk()
+        {
+            _productSale.Object.Id = 1;
+            _productSale.Object.CostPrice = 2;
+            _productSale.Object.SalePrice = 5;
+            _sale = ObjectMother.GetSale(_productSale.Object);
+            var profit = 9;
+            _sale.Validate();
+
+            _sale.Profit.Should().Be(profit);
+            _sale.Client.Should().Be("Bruno Ribeiro");
+            _sale.ProductSale.Should().Be(_productSale.Object);
+        }
+
+        [Test]
+        public void Sale_InvalidClient_ShouldFail()
+        {
+            _sale = ObjectMother.GetSaleEmptyClient(_productSale.Object);
+
+            Action Act = () => _sale.Validate();
+
+            Act.Should().Throw<SaleEmptyClienteException>();
+        }
+
+        [Test]
+        public void Sale_InvalidAmount_ShouldFail()
+        {
+            _sale = ObjectMother.GetSaleEmptyAmount(_productSale.Object);
+
+            Action Act = () => _sale.Validate();
+
+            Act.Should().Throw<SaleEmptyOrZeroAmountException>();
+        }
+
+        [Test]
+        public void Sale_InvalidProductSale_ShouldFail()
+        {
+            _sale = ObjectMother.GetSaleEmptyProductSale();
+
+            Action Act = () => _sale.Validate();
+
+            Act.Should().Throw<SaleEmptyOrNullProductSaleException>();
+        }
+
+        [TearDown]
+        public void TearDonw()
+        {
+            _sale = null;
+            _productSale = null;
+        }
+
+    }
+}
---
+++ DonaLaura/DonaLaura.Domain/Features/Sales/Sale.cs
@@ -0,0 +1,25 @@
+﻿using DonaLaura.Domain.Features.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonaLaura.Domain.Features.Sales
+{
+    public class Sale
+    {
+        public long Id { get; set; }
+        public Product ProductSale { get; set; }
+        public string Client { get; set; }
+        public int Amount { get; set; }
+        public double Profit { get { return Profit = (ProductSale.SalePrice - ProductSale.CostPrice) * Amount; } set { } }
+
+        public void Validate()
+        {
+            if (String.IsNullOrEmpty(Client)) throw new SaleEmptyClienteException();
+            if (Amount <= 0) throw new SaleEmptyOrZeroAmountException();
+            if (ProductSale == null) throw new SaleEmptyOrNullProductSaleException();
+        }
+    }
+}
