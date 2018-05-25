b66cb36e2572e83ed7542f1e933f7b2bb9490126
Bruno Wagner <brunowagner62@gmail.com>
2018-05-25 01:27:47 -0300

    Criado dominio de Emprestimo

---
+++ DonaRosangela/DonaRosangela.Domain/Features/Loans/Loan.cs
@@ -0,0 +1,18 @@
+﻿using DonaRosangela.Domain.Features.Books;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonaRosangela.Domain.Features.Loans
+{
+    public class Loan
+    {
+        public string Client { get; set; }
+        public Book LoanBook { get; set; }
+        public DateTime Devolution { get; set; }
+
+        void Validade() { }
+    }
+}
