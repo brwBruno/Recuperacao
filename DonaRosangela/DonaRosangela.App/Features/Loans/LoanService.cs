d6e4c8991f78eb9456f2e60661615a771a4e6568
Bruno Wagner <brunowagner62@gmail.com>
2018-05-25 02:40:01 -0300

    Criado interfaces de repositorio e servico de emprestimo, e criado a classe de servico

---
+++ DonaRosangela/DonaRosangela.App/Features/Loans/LoanService.cs
@@ -0,0 +1,43 @@
+﻿using DonaRosangela.Domain.Features.Loans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonaRosangela.App.Features.Loans
+{
+    public class LoanService : ILoanService
+    {
+        ILoanRepository _repository;
+        public LoanService(ILoanRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Loan Add(Loan loan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Loan loan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Loan Get(Loan loan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Loan> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Loan Update(Loan loan)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
